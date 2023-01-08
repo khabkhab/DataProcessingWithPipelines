cd5dface57b65833133abe62ca718e15a01a7236
khabkhab <khabibyusupov2014@gmail.com>
2023-01-08 16:27:21 +0100

    Nan issue is fixed; added structure and name to main

--- Processing_with_pipelines.py
+++ Processing_with_pipelines.py
@@ -5,28 +5,31 @@ import matplotlib.pyplot as plt
 import geopandas
 
 
-#drops the first column that has no name
+#drops the first column that has no name and delete rows where "NaN" is present
 class DropNames(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
     def transform(self, X):
-        return X.drop(["Unnamed: 0", "Country Code", "Series Name", "Series Code"], axis=1)
-
+        X = X.drop(["Unnamed: 0", "Country Code", "Series Name", "Series Code"], axis=1)
+        X.dropna(how='any')
+        return X
+#group the dataframe by Country name and sums/mean = sum()/mean() the dept
 class GroupBy(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
     def transform(self, X):
-        X = X.groupby('Country Name').mean()
+        X = X.groupby('Country Name').sum()
         return X
     
-
-data = pd.read_csv("ids_last_5y.csv")
-df = pd.DataFrame(data)
-
+#combining pipeline 
 pipe = Pipeline([
     ("dropper", DropNames()),
     ("groupper", GroupBy()) 
-])
-result = pd.DataFrame(pipe.fit_transform(df))
-print(result)
+])   
+if __name__ == "__main__":
+    data = pd.read_csv("ids_last_5y.csv")
+    df = pd.DataFrame(data)
+    #converts ouput from pipeline back to dataframe format
+    result = pd.DataFrame(pipe.fit_transform(df))
+    print(result)
 
